289cdf002c02bda690986735f4b1cf15ea1b07fe
Juanwei Hu <jha387@sfu.ca>
2024-03-07 00:16:57 -0800

    try update test and delete rvg files

--- src/test/Test.jsx
+++ src/test/Test.jsx
@@ -3,7 +3,7 @@ function Test(){
     return (
 
         <div>
-            <h1>Create a sample string for display</h1>
+            <h1>Create a sample string for display This is test purpose only</h1>
         </div>
     )
 }
