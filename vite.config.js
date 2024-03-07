fc1382c4bc381723da2abe14c6d10d8c486ddd43
Juanwei Hu <jha387@sfu.ca>
2024-03-07 01:15:57 -0800

    try update test and delete rvg files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // or '/<repository-name>/'
   build: {
-    outDir: 'build',
+    outDir: 'dist',
   },
 })
